192bcb80b43af1158edc91fad086b6d67b777bf3
alexe14ernyakov <alexe14ernyakov@mail.ru>
2023-10-04 09:17:23 +0300

    Added all basic operations and trigonometrical form of complex numbers

--- complex.cpp
+++ complex.cpp
@@ -3,16 +3,28 @@
 math::complex::complex(int number) {
     _x = (double)number;
     _y = 0;
+    _r = number;
+    if(number >= 0)
+        _phi = 0;
+    else
+        _phi = 1.0;
 }
 
 math::complex::complex(double number) {
     _x = number;
     _y = 0;
+    _r = number;
+    if(number >= 0)
+        _phi = 0;
+    else
+        _phi = 1.0;
 }
 
 math::complex::complex(double x, double y) {
     _x = x;
     _y = y;
+    _r = sqrt(_x * _x + _y * _y);
+    _phi = atan(_y/_x);
 }
 
 const math::complex math::operator+(const math::complex &first, const math::complex &second) {
@@ -22,4 +34,23 @@ const math::complex math::operator+(const math::complex &first, const math::comp
 const math::complex math::operator-(const math::complex &first, const math::complex &second) {
     return math::complex(first._x - second._x, first._y - second._y);
 }
+
+const math::complex math::operator*(const math::complex &first, const math::complex &second) {
+    return math::complex(first._x * second._x - first._y * second._y, first._x * second._y + first._y * second._x);
+}
+
+const math::complex math::operator/(const math::complex &first, const math::complex &second) {
+    if(second.modulus() == 0)
+        throw std::invalid_argument("Division by zero");
+
+    double x = (first._x * second._x + first._y * second._y)/(second._x * second._x + second._y + second._y);
+    double y = (second._x * first._y - first._x * second._y)/(second._x * second._x + second._y + second._y);
+    return complex(x, y);
+}
+
+double math::complex::modulus() const {
+    return _r;
+}
+
+
 
--- complex.h
+++ complex.h
@@ -1,11 +1,15 @@
 #ifndef MATH_COMPLEX_H
 #define MATH_COMPLEX_H
+#include <cmath>
+#include <stdexcept>
 
 namespace math {
     class complex {
     private:
         double _x = 0;
         double _y = 0;
+        double _r = 0;
+        double _phi = 0;
     public:
         explicit complex(int number);
         explicit complex(double number);
@@ -13,6 +17,10 @@ namespace math {
 
         friend const complex operator+(const complex& first, const complex& second);
         friend const complex operator-(const complex& first, const complex& second);
+        friend const complex operator*(const complex& first, const complex& second);
+        friend const complex operator/(const complex& first, const complex& second);
+
+        [[nodiscard]] double modulus() const;
     };
 }
 #endif
